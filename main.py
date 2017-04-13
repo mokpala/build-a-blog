89b38cfe97d2409cdf1ca1041efd83520dd10c30
Michael Okpala <maokpala@gmail.com>
2017-04-13 16:09:53 -0500

    Added permalinks to existing posts

--- main.py
+++ main.py
@@ -16,34 +16,57 @@ class Handler(webapp2.RequestHandler):
         return t.render(params)
         
     def render(self, template, **kw):
-        self.write(self.render_str(template, **kw))
+        self.write(self.render_str(template, **kw))        
 
 class Post(db.Model):
     title = db.StringProperty(required = True)
     post = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
         
-class MainHandler(Handler):
-    def render_front(self, title="", post="", error=""):
+# class MainHandler(Handler):
+    # def render_front(self, title="", post="", error=""):
+        # posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC")
+        # self.render("blog.html", title=title, post=post, error=error, posts=posts)
+        
+    # def get(self):
+        # self.render_front()
+        
+class BlogMain(Handler):
+    def get_blog(self, title="", post="", error=""):
         posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC")
-        self.render("newpost.html", title=title, post=post, error=error, posts=posts)
+        self.render("blog.html", title=title, post=post, error=error, posts=posts)
         
     def get(self):
-        self.render_front()
+        self.get_blog()
         
+class NewPost(Handler):
+    def new_post(self, title="", post="", error=""):
+        self.render("newpost.html", title=title, post=post, error=error)
+        
+    def get(self):
+        self.new_post()
+    
     def post(self):
         title = self.request.get("title")
         post = self.request.get("post")
         
         if title and post:
             p = Post(title = title, post = post)
-            p.put()
+            key = p.put()
+            self.redirect("/blog/%d" % key.id())
             
-            self.redirect("/")
         else:
             error = "We need both a title and a post."
-            self.render_front(title, post, error)
+            self.new_post(title, post, error)      
+
+class BlogPage(Handler):
+    def get(self, id):
+        s = Post.get_by_id(int(id))
+        self.render("blogpage.html", posts = [s])
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    # ('/', MainHandler),
+    ('/blog', BlogMain),
+    ('/blog/newpost', NewPost),
+    webapp2.Route('/blog/<id:\d+>', BlogPage)
 ], debug=True)
